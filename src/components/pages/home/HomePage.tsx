bfefb01288d2961cc5bd22f7739c6ed3fc6e4250
ginpei <ginpei@ginpei.dev>
2021-04-27 00:21:27 -0700

    feat: 🎸 styles

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const HomePage: React.FC = () => {
   return (
-    <div className="HomePage">
+    <div className="HomePage u-container">
       <h1>HomePage</h1>
       <p>
         <Link to="/about/">About</Link>
