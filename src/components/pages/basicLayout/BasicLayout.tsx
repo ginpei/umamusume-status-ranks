fb26e8095c59ac87f840f9a0149695742b04393a
ginpei <ginpei@ginpei.dev>
2021-04-28 09:49:52 -0700

    feat: 🎸 root path

--- src/components/pages/basicLayout/BasicLayout.tsx
+++ src/components/pages/basicLayout/BasicLayout.tsx
@@ -1,7 +1,12 @@
 import { GpBasicLayout } from "../../../gp/components/pages/basicLayout/GpBasicLayout";
+import { rootPath } from "../../../misc";
 
 export const BasicLayout: React.FC = ({ children }) => {
   return (
-    <GpBasicLayout appName="ウマ娘評価" children={children} homePath="/" />
+    <GpBasicLayout
+      appName="ウマ娘評価"
+      children={children}
+      homePath={rootPath()}
+    />
   );
 };
---
+++ src/misc.ts
@@ -0,0 +1,3 @@
+export function rootPath(): string {
+  return "/";
+}
