64a09af213a6bb1c4b170aa73677f092a41324ca
ginpei <ginpei@ginpei.dev>
2021-04-29 09:37:03 -0700

    refactor: 💡 extract AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 import { CurrentUserProvider } from "./data/CurrentUserContext";
 import { useFirebaseCurrentUser } from "./data/useFirebaseCurrentUserHook";
 import { auth } from "./gp-firebase/firebase";
-import { rootPath } from "./misc";
-import {
-  EntryListPage,
-  entryListPagePath,
-} from "./ui/screens/entryList/EntryListPage";
-import { HomePage } from "./ui/screens/home/HomePage";
-import { LoginPage, loginPagePath } from "./ui/screens/login/LoginPage";
-import { NotFoundScreen } from "./ui/screens/misc/NotFoundScreen";
-import {
-  RaceListPage,
-  raceListPagePath,
-} from "./ui/screens/raceList/RaceListPage";
-import {
-  RegisterPage,
-  registerPagePath,
-} from "./ui/screens/register/RegisterPage";
+import { AppRouter } from "./mist/AppRouter";
 
 const App: React.FC = () => {
   const user = useFirebaseCurrentUser(auth);
@@ -28,28 +13,7 @@ const App: React.FC = () => {
 
   return (
     <CurrentUserProvider value={user}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact={true} path={rootPath()} component={HomePage} />
-          <Route exact={true} path={loginPagePath()} component={LoginPage} />
-          <Route
-            exact={true}
-            path={registerPagePath()}
-            component={RegisterPage}
-          />
-          <Route
-            exact={true}
-            path={raceListPagePath()}
-            component={RaceListPage}
-          />
-          <Route
-            exact={true}
-            path={entryListPagePath(":raceTitle")}
-            component={EntryListPage}
-          />
-          <Route component={NotFoundScreen} />
-        </Switch>
-      </BrowserRouter>
+      <AppRouter />
     </CurrentUserProvider>
   );
 };
---
+++ src/mist/AppRouter.tsx
@@ -0,0 +1,45 @@
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { rootPath } from "../misc";
+import {
+  EntryListPage,
+  entryListPagePath,
+} from "../ui/screens/entryList/EntryListPage";
+import { HomePage } from "../ui/screens/home/HomePage";
+import { LoginPage, loginPagePath } from "../ui/screens/login/LoginPage";
+import { NotFoundScreen } from "../ui/screens/misc/NotFoundScreen";
+import {
+  RaceListPage,
+  raceListPagePath,
+} from "../ui/screens/raceList/RaceListPage";
+import {
+  RegisterPage,
+  registerPagePath,
+} from "../ui/screens/register/RegisterPage";
+
+export const AppRouter: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path={rootPath()} component={HomePage} />
+        <Route exact={true} path={loginPagePath()} component={LoginPage} />
+        <Route
+          exact={true}
+          path={registerPagePath()}
+          component={RegisterPage}
+        />
+        <Route
+          exact={true}
+          path={raceListPagePath()}
+          component={RaceListPage}
+        />
+        <Route
+          exact={true}
+          path={entryListPagePath(":raceTitle")}
+          component={EntryListPage}
+        />
+        <Route component={NotFoundScreen} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
