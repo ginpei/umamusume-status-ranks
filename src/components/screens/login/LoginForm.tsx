6b96bf4b79d2ef4547cfc3dad06277328b4e3d2c
ginpei <ginpei@ginpei.dev>
2021-04-28 21:06:27 -0700

    feat: 🎸 login for emu

--- src/components/screens/login/LoginForm.tsx
+++ src/components/screens/login/LoginForm.tsx
@@ -2,16 +2,29 @@ import firebase from "firebase/app";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import { auth } from "../../../gp-firebase/firebase";
 
+const signInOptions = [
+  firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+  firebase.auth.EmailAuthProvider.PROVIDER_ID,
+];
+
+// Twitter log in does not seem to work with emulator?
+// I need another way
+const withEmulator = process.env.REACT_APP_WITH_EMULATOR;
+if (withEmulator) {
+  signInOptions.push(firebase.auth.GithubAuthProvider.PROVIDER_ID);
+}
+
 const uiConfig: firebaseui.auth.Config = {
   callbacks: {
+    signInFailure(error) {
+      console.log(`# fail`, error);
+    },
     signInSuccessWithAuthResult() {
+      console.log(`# success`);
       return false;
     },
   },
-  signInOptions: [
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-  ],
+  signInOptions,
 };
 
 export const LoginForm: React.FC = () => {
