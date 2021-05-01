81ecb793d0a014744d68d15591a80444460d43f6
ginpei <ginpei@ginpei.dev>
2021-05-01 01:50:56 -0700

    refactor: 💡 reuse prepared function

--- src/data/raceEntryHook.ts
+++ src/data/raceEntryHook.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Firestore } from "../gp-firebase/firebase";
 import { RaceEntry } from "./RaceEntry";
-import { getRaceEntryCollection, ssToRaceEntry } from "./RaceEntryDb";
+import { getRecentRaceEntriesQuery, ssToRaceEntry } from "./RaceEntryDb";
 
 export function useRaceEntriesByTitle(
   db: Firestore,
@@ -10,8 +10,7 @@ export function useRaceEntriesByTitle(
   const [entries, setEntries] = useState<RaceEntry[] | null>(null);
 
   useEffect(() => {
-    const coll = getRaceEntryCollection(db);
-    const query = coll.where("raceTitle", "==", title).orderBy("createdAt");
+    const query = getRecentRaceEntriesQuery(db, title);
     return query.onSnapshot((ss) => {
       const newEntries = ss.docs.map((v) => ssToRaceEntry(v));
       setEntries(newEntries);
