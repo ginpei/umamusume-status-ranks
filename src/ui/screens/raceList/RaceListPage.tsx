eeaf35bb6df068763bec69de109dd229c159e10e
ginpei <ginpei@ginpei.dev>
2021-05-23 21:49:51 -0700

    feat: 🎸 link to data source

--- src/ui/screens/raceList/RaceListPage.tsx
+++ src/ui/screens/raceList/RaceListPage.tsx
@@ -29,6 +29,15 @@ export const RaceListPage: React.FC = () => {
   return (
     <BasicLayout title="レース一覧">
       <h1>レース一覧</h1>
+      <p>
+        レース情報：
+        <a
+          href="https://docs.google.com/spreadsheets/d/1iNbk3SnfOGxE1NE-FYXQCK6DQ0VrAE2REVRbZ488CjY/edit#gid=360769166"
+          target="_blank"
+        >
+          【ウマ娘】全レースカレンダー(ウマ娘目標付き) ver1.2.1 - Google Sheets
+        </a>
+      </p>
       <details open={filterFormOpen}>
         <summary>フィルター</summary>
         <RaceFilterForm filter={filter} onChange={onFilterChange} />
